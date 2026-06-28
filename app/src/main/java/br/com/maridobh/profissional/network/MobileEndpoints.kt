d2d6e39446ee948746d2a76640ed9b0081c87ed3
nosborzone-cmyk <nosborzone@gmail.com>
2026-06-28 02:04:01 -0300

    Delete app directory

--- app/src/main/java/br/com/maridobh/profissional/network/MobileEndpoints.kt
+++
@@ -1,9 +0,0 @@
-package br.com.maridobh.profissional.network
-
-import br.com.maridobh.profissional.AppConfig
-import br.com.maridobh.profissional.BuildConfig
-
-object MobileEndpoints {
-    val registerDeviceUrl: String
-        get() = AppConfig.baseUrl + BuildConfig.MOBILE_DEVICE_ENDPOINT
-}
